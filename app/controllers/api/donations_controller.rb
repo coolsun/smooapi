0c0a79ca2c66a15932d502271466a1d08cb02918
sunb0930 <ben@trillioninnovations.com>
2021-03-02 14:58:10 -0800

    my donations

--- app/controllers/api/donations_controller.rb
+++ app/controllers/api/donations_controller.rb
@@ -1,6 +1,6 @@
 class Api::DonationsController < Api::BaseController
     def index
-        all_donations = Donation.all
+        all_donations = current_user.donations
         render json: all_donations.to_json
     end
 
