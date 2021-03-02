f8a9b55ca4450dc4666b135edcf99ea477397e98
sunb0930 <ben@trillioninnovations.com>
2021-03-01 23:26:12 -0800

    campaign fix

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,6 +2,5 @@ class Campaign < ApplicationRecord
     belongs_to :user
     belongs_to :category
     has_many :comments
-    has_many :campaign_donations, dependent: :delete_all
-    has_many :donations, through: :campaign_donations
+    has_many :donations
 end
