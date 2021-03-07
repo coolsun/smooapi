34485dcb488e04e6f0551471165b86dcecd3e430
sunb0930 <ben@trillioninnovations.com>
2021-03-06 18:12:51 -0800

    add campagin virtual columns - campaign_started, goal_reached.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,4 +3,13 @@ class Campaign < ApplicationRecord
     belongs_to :category
     has_many :comments
     has_many :donations
+    attr_accessor :campaign_started, :goal_reached
+
+    def campaign_started
+        (Time.now - self.created_at).to_i/86400.to_i
+    end
+
+    def goal_reached
+        self.donations.sum(:amount) / self.goal 
+    end
 end
