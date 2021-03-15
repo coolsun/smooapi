55092270bf249f598ec63c16e6edd0f17b31c10b
sunb0930 <ben@trillioninnovations.com>
2021-03-14 22:25:30 -0700

    permits fn, ln sent by sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def create
     build_resource(sign_up_params)
@@ -8,6 +9,11 @@ class RegistrationsController < Devise::RegistrationsController
     @url = "http://www.101moo.com"
     UserMailer.with(user: @user).welcome_email.deliver_later
     render_jsonapi_response(resource)
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
   end
 
 end
